403a5b5388cc7a18632e94eaf12f66a819716ad8
Samuel Bassett <samabassett@gmail.com>
2020-08-10 10:18:07 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>PETER MOREAU</title>
+        <link rel="icon" href="/images/favicon_io/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+
+      <style jsx>{``}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
