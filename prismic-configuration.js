73baf1d9adc79dc6b406008a233324e8598c12b6
Samuel Bassett <samabassett@gmail.com>
2020-08-13 08:32:03 +0200

    update

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,10 +1,8 @@
-const REPOSITORY = process.env.PRISMIC_REPOSITORY_NAME;
-
 module.exports = {
   // -- Prismic API endpoint
   // Determines which repository to query and fetch data from
   // Configure your site's access point here
-  apiEndpoint: `https://${REPOSITORY}.prismic.io/api/v2`,
+  //   apiEndpoint: `https://${REPOSITORY}.prismic.io/api/v2`,
 
   // -- Access Token if the repository is not public
   // Generate a token in your dashboard and configure it here if your repository is private
